dd7f377de32959c5cb97620c460c140cf84ac3c3
roneissu <sadeluisronaldo@gmail.com>
2019-08-03 23:27:35 -0300

    Updates

---
+++ precache-manifest.ab87d2191ffd32c3e09d5da50bf54f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e697e870c0943d9ac24c40eb87ef86f",
+    "url": "/rent-clothes/index.html"
+  },
+  {
+    "revision": "71bce130899f0452dd46",
+    "url": "/rent-clothes/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rent-clothes/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "71bce130899f0452dd46",
+    "url": "/rent-clothes/static/js/main.efe8b71e.chunk.js"
+  },
+  {
+    "revision": "5eb8b5f88c627247bcec",
+    "url": "/rent-clothes/static/js/runtime~main.a64c2cb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rent-clothes/static/media/logo.5d5d9eef.svg"
+  }
+]);
