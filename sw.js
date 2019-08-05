dc3d1dc743cc1175d0a73612d20c7a206e61d481
roneissu <sadeluisronaldo@gmail.com>
2019-08-05 01:57:09 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
 const CACHE_NAME = 'rent-clothes';
 
 const urlsToCache = [
-    './'
+    './',
+    './favicon.ico',
+    './index.html',
+    './static',
+    './static/js',
+    './static/js/2.c2695c00.chunk.js',
+    './static/js/2.c2695c00.chunk.js.map',
+    './static/js/main.281031c3.chunk.js',
+    './static/js/main.281031c3.chunk.js.map',
+    './static/js/runtime~main.a64c2cb9.js',
+    './static/js/runtime~main.a64c2cb9.js.map',
+    './static/css',
+    './static/css/main.1c933296.chunk.css',
+    './static/css/main.1c933296.chunk.css.map',
+    './images',
+    './images/icon-144x144.png',
+    './images/calendario.png',
+    './images/lapis.png',
+    './images/vestido.png',
+    './images/seta_curva.svg'
 ];
 
 this.addEventListener('install', function (event) {
