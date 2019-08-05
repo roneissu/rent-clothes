9dd3897ff4e3f88328b27c9084e716dbde2c78b9
roneissu <sadeluisronaldo@gmail.com>
2019-08-05 01:02:12 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
 const CACHE_NAME = 'rent-clothes';
 
 const urlsToCache = [
-    '/rent-clothes/',
-    '/rent-clothes/static/js/bundle.js',
-    '/rent-clothes/static/js/main.chunk.js',
-    '/rent-clothes/static/js/1.chunk.js',
-    '/rent-clothes/static/js/0.chunk.js',
-    '/rent-clothes/favicon.ico',
-    '/rent-clothes/css/main.0df1030a.chunk.css',
-    '/rent-clothes/css/main.0df1030a.chunk.css.map',
-    '/rent-clothes/icons/icon-144x144.png',
-    '/rent-clothes/icons/icon-512x512.png',
-    '/rent-clothes/images/calendario.png',
-    '/rent-clothes/images/lapis.png',
-    '/rent-clothes/images/vestido.png',
-    '/rent-clothes/images/seta_curva.svg'
+    './',
+    './static/js/bundle.js',
+    './static/js/main.chunk.js',
+    './static/js/1.chunk.js',
+    './static/js/0.chunk.js',
+    './favicon.ico',
+    // './css/main.0df1030a.chunk.css',
+    // './css/main.0df1030a.chunk.css.map',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    './images/calendario.png',
+    './images/lapis.png',
+    './images/vestido.png',
+    './images/seta_curva.svg'
 ];
 
 this.addEventListener('install', function (event) {
